ff15196d5da22ef77c27fde074239d27d4423829
dpgallivan <danielgallivan4@gmail.com>
2017-08-14 16:27:33 -0700

    Update BasicCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -1,37 +1,12 @@
-var fs = require('fs');
-var inquirer = require('inquirer');
+function BasicCard(front, back) {
 
-function BasicCard(front, back){
-	this.front = front;
-	this.back = back;
+  // This code allows us to optionally create BasicCards without using the new keyword
+  if (!(this instanceof BasicCard)) {
+    return new BasicCard(front, back);
+  }
+
+  this.front = front;
+  this.back = back;
 }
-
-BasicCard.prototype.printInfo = function(){
-	console.log('Front: ' + this.front + 'Back: ' + this.back);
-};
-
-var answersRight = 0;
-var answersWrong = 0;
-
-var basicQuestions = function(){
-	if(answersRight < 3){
-		inquirer.prompt([
-		{
-			name: 'front',
-			message: 'Who is the first president of the United States?'
-		},{
-			name: 'back',
-			message: 'George Washington'
-		}]).then(function(answers){
-			if(answers.message === 'George Washington'){
-				answersRight++;
-				console.log("Correct!");
-			} else {
-				answersWrong++;
-			}
-		});
-	}
-};
-basicQuestions();
 
 module.exports = BasicCard;
